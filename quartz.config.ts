fcbddfce54821ad5855611196f0f0e9a8541cffc
allwayso <1784817625@qq.com>
2026-05-27 14:50:39 +0800

    fix:change the base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "allwayso.github.io/cs231n",
+     baseUrl: "https://allwayso.github.io/cs231n",
     ignorePatterns: ["private", "templates", ".obsidian"],
     theme: {
       fontOrigin: "googleFonts",
